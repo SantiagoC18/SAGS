de290b1f78fb1b6ec4a63d17a4d5da201150e1d4
SantiagoC18 <143475795+SantiagoC18@users.noreply.github.com>
2024-07-31 11:39:13 -0500

    Add files via upload

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='templates')
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='sirs'
+app.config['MYSQL_DB']='sags'
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 mysql=MySQL(app)
 
@@ -32,15 +32,14 @@ def acceso_login():
         clave = request.form['clave']
         
         cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM usuarios WHERE correo = %s AND  password = %s LIMIT 1', (correo, clave,))
+        cur.execute('SELECT * FROM usuarios WHERE email = %s AND  password = %s LIMIT 1', (correo, clave,))
         account = cur.fetchone()
         
         if account:
             
             session['logueado'] = True
-            session['correo'] = account['correo']
-            session['nombre'] = account['primer_nombre']
-            session['id'] = account['id_usuario']
+            session['nombre'] = account['nombres']
+            session['id'] = account['email']
             
             return redirect(url_for('perfil'))
         else:
@@ -61,21 +60,20 @@ def adduser():
         
     if id_user and correo and clave:
         cur = mysql.connection.cursor()
-        sql = "INSERT INTO `usuarios`(`id_usuario`, `password`, `correo`, `cod_rol1`) VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO `usuarios`(`documento`, `password`, `email`, `cod_rol1`) VALUES (%s, %s, %s, %s)"
         data = (id_user, clave, correo, rol)
         cur.execute(sql,data)
         mysql.connection.commit()
         
         
-        cur.execute('SELECT * FROM usuarios WHERE correo = %s AND  password = %s LIMIT 1', (correo, clave,))
+        cur.execute('SELECT * FROM usuarios WHERE email = %s AND  password = %s LIMIT 1', (correo, clave,))
         account = cur.fetchone()
         
         if account:
             
             session['logueado'] = True
-            session['correo'] = account['correo']
-            session['nombre'] = account['primer_nombre']
-            session['id'] = account['id_usuario']
+            session['nombre'] = account['nombres']
+            session['id'] = account['email']
             
             return redirect(url_for('perfil'))
         else:
@@ -98,17 +96,17 @@ def modulos():
 def perfil():
     if session.get('logueado'):
         cur = mysql.connection.cursor()
-        consulta = "SELECT * FROM usuarios WHERE id_usuario = %s"
+        consulta = "SELECT * FROM usuarios WHERE email = %s"
         id = session['id']
         cur.execute(consulta, (id,))
         data = cur.fetchall()
         
         cur.execute(''' SELECT * FROM proyectos
-				        INNER JOIN p_i
-				        ON proyectos.id_proyecto = p_i.id_proyecto1
+				        INNER JOIN usu_proy
+				        ON proyectos.idproy = usu_proy.idproy
 				        INNER JOIN usuarios
-				        ON usuarios.id_usuario = p_i.id_usuario1
-				        WHERE id_usuario = %s;''',(id,))
+				        ON usuarios.email = usu_proy.email
+				        WHERE usu_proy.email = %s;''',(id,))
         data2 = cur.fetchall()
             
         return render_template('perfil.html', usuario = data, datos = data2, log='Cerrar')
