eddfd59ccdb1a64e56987f15cc19c2abbcf0c798
Shiuu28 <shiuuvp28@outlook.com>
2025-01-31 17:58:32 -0500

    Feat: update readme.md

--- static/js/editor_options.js
+++ static/js/editor_options.js
@@ -6,6 +6,30 @@ function addShape(type) {
     shape.draggable = false;
 
     document.getElementById('edit').appendChild(shape);
-    makeDraggable(shape, resizer);
+    makeDraggable(shape);
 }
 
+function makeDraggable(element) {
+    let ejeX, ejeY, isDragging = false;
+
+    element.addEventListener("mousedown", (e) => {
+        isDragging = true;
+        const rect = element.getBoundingClientRect();
+        ejeX = e.clientX - rect.left;
+        ejeY = e.clientY - rect.top;
+        element.style.cursor = 'grabbing';
+    });
+
+    document.addEventListener("mousemove", (e) => {
+        if (!isDragging) return;
+        element.style.position= 'absolute';
+        element.style.left = `${e.clientX - ejeX}px`;
+        element.style.top = `${e.clientY - ejeY}px`;
+
+    });
+
+    document.addEventListener("mouseup", () => {
+        isDragging = false;
+        element.style.cursor = 'grab';
+    });
+}
