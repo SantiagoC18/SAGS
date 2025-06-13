bb689f63add64d99d344814fbb0bad4914f6589c
SantiagoC18 <santicardenash@gmail.com>
2025-06-13 00:08:48 -0500

    Preparación para despliegue en Heroku

--- config/config.py
+++ config/config.py
@@ -1,18 +1,22 @@
+import os
+
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'sags'
+    # Configuración para base de datos
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'sags')
     MYSQL_CURSORCLASS = 'DictCursor'
     
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = 'softwareanalysissa@gmail.com'
-    MAIL_PASSWORD = 'uahw rsnt nqko kzyb'
+    # Configuración para correo
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 465))
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'softwareanalysissa@gmail.com')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'uahw rsnt nqko kzyb')
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     
-    SECRET_KEY = "4546416vblñvkbmgvlñkbjfgñfglñv.ñ"
+    SECRET_KEY = os.environ.get('SECRET_KEY', "4546416vblñvkbmgvlñkbjfgñfglñv.ñ")
 
 class TestingConfig(Config):
     TESTING = True
